b32a6da81bbba85b9b53db012af5bafbc584791f
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2023-06-01 00:34:04 +0530

    feat : go loops

--- 01.go
+++ 01.go
@@ -2,7 +2,14 @@ package main
 
 import "fmt"
 
-func main(){
+func main1(){
+
+	var num1 int = 3
+	var num2 int = 3
+	
+	fmt.Println(num1 + num2)
+
 	fmt.Println("Hello Gooooooooooooooooooooooooooo")
+	fmt.Println(2+3)
 }
 
---
+++ 02.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main2(){
+
+	var num1, num2 int = 21,31;
+	
+	fmt.Println(num1 + num2)
+
+	result := 9 // var result int = 9
+
+	fmt.Println(result)
+
+
+	const check int = 9 // var result int = 9
+
+	fmt.Println(check)
+}
+
